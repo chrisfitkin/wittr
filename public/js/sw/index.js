1c48daa744438f3f74c248d3fad6b51187d48906
Chris Fitkin <cfitkin@jnrcorp.com>
2016-10-19 14:50:31 -0700

    feat: Override .jpg with dr-evil.gif

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,11 @@
 self.addEventListener('fetch', function(event) {
-	// TODO: respond to all requests with an html response
-	// containing an element with class="a-winner-is-me".
-	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  // console.log('------ Match Event ------');
+  // console.log(event.request.url);
+
+  if (/.*\.jpg$/.test(event.request.url)) {
+    event.respondWith(
+      fetch('/imgs/dr-evil.gif')
+    )
+  }
+
 });
